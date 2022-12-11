0ce35c61dcefb2c29bdc6c4b88a4fb7fbfb47daf
“Salil48” <salilchharia@gmail.com>
2022-12-11 17:47:07 +0530

    Added: Computation4

--- sortingarithmetic.sh
+++ sortingarithmetic.sh
@@ -4,6 +4,6 @@ read -p "enter first value : " a;
 read -p "enter second value : " b;
 read -p "enter third value : " c;
 
-y=$(($c+($a/$b)));
-echo "$c + $a/$b = $y";
+z=$((($a%$b)+$c));
+echo "$a%$b + $c = $z";
 
