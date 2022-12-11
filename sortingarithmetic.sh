77b0ed8c15ec028a7f47f8e8862789056a123df1
“Salil48” <salilchharia@gmail.com>
2022-12-11 17:36:16 +0530

    Added: Computation3

--- sortingarithmetic.sh
+++ sortingarithmetic.sh
@@ -4,6 +4,6 @@ read -p "enter first value : " a;
 read -p "enter second value : " b;
 read -p "enter third value : " c;
 
-x=$((($a*$b)+$c));
-echo "$a*$b + $c = $x";
+y=$(($c+($a/$b)));
+echo "$c + $a/$b = $y";
 
