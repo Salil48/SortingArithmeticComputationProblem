bc2f1aa67d6fc427467e5b9fa420ae332c0d3f07
“Salil48” <salilchharia@gmail.com>
2022-12-11 17:58:17 +0530

    Added: Dictionary

--- sortingarithmetic.sh
+++ sortingarithmetic.sh
@@ -4,6 +4,19 @@ read -p "enter first value : " a;
 read -p "enter second value : " b;
 read -p "enter third value : " c;
 
+declare -A w
+w=$(($a+($b*$c)));
+echo "$a + $b*$c = $w";
+
+declare -A x
+x=$((($a*$b)+$c));
+echo "$a*$b + $c = $x";
+
+declare -A y
+y=$(($c+($a/$b)));
+echo "$c + $a/$b = $y";
+
+declare -A z
 z=$((($a%$b)+$c));
 echo "$a%$b + $c = $z";
 
