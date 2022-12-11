eec9cd2abc6d663c0f868f1e814fb51c27b641ea
“Salil48” <salilchharia@gmail.com>
2022-12-11 19:32:26 +0530

    Added: Sort in Decending Order

--- sortingarithmetic.sh
+++ sortingarithmetic.sh
@@ -22,3 +22,23 @@ echo "$a%$b + $c = $z";
 
 arr=($w $x $y $z)
 echo ${arr[@]}
+
+read -p "Enter The Number: "  n
+for((i=0; i<$n; i++))
+do
+read -p "Enter value of arr[$i]: " arr[$i]
+done
+#sorting code
+for((i=0; i<$n; i++))
+do
+for((j=0; j<n-i-1; j++))
+do
+if [ ${arr[j]} -lt ${arr[$((j+1))]} ]
+then
+#swapping
+temp=${arr[j]}
+arr[$j]=${arr[$((j+1))]}
+arr[$((j+1))]=$temp
+fi
+done
+echo "Numbers in Descending order: " ${arr[@]}
